adf72397705447c3993bb2a6451616275316b4da
brakenium <brakenium@example.com>
2023-06-24 11:37:44 +0200

    Revert "Remove own Fromstr implementations"
    This reverts commit 48d2f0ea

--- auraxis/src/realtime/client.rs
+++ auraxis/src/realtime/client.rs
@@ -268,6 +268,7 @@ impl RealtimeClient {
     ) -> Result<(), AuraxisError> {
         match msg {
             Message::Text(text) => {
+                info!("Received: {}", text);
                 let message: CensusMessage = serde_json::from_str(&text)?;
 
                 match message {
